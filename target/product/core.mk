f3b68fadf340cb759e16fe1f6bdcbafb44eac905
mailoxy <github@nohup.xyz>
2019-02-09 23:42:20 +0300

    Add Backup app

--- target/product/core.mk
+++ target/product/core.mk
@@ -61,7 +61,8 @@ PRODUCT_PACKAGES += \
     vr \
     MmsService \
     chromium \
-    Updater
+    Updater \
+    Backup
 
 # The set of packages whose code can be loaded by the system server.
 PRODUCT_SYSTEM_SERVER_APPS += \
